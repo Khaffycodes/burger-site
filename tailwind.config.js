b83c481a57e5144d2bdd4ace420a6246bb064034
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-20 18:52:03 +0100

    edited files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-    // './app/**/*.{js,ts,jsx,tsx}',
-    // './pages/**/*.{js,ts,jsx,tsx}',
-    // './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#010a5e',
+        primaryColorLight: '#010d78',
+        secondaryColor: '#FFCC00',
+        paragraphColor: '#c0c0c0',
+        whiteColor: '#fff',
+        blackColor: '#000',
+        greenColor: '#007936',
+        redColor: '#cc3433',
+        darkColor: '#000',
+        darkColorLight: '#171717',
+      },
+    },
   },
   plugins: [],
 };
